489c9f1a6a9b9b0d18c5c189a894a8b281c253ae
Alan Bell <alanbell@apertum.ie>
2024-11-29 14:29:43 +0000

    clickable photos

--- about.md
+++ about.md
@@ -1,1 +1,9 @@
-How to use this tool . . .
+# Hashflow
+## _Bluesky Hashtags on stream_
+
+Hashflow is a way to passively watch activity on the Bluesky social network organised by hashtags. You can type any hashtag (with or without the #) into the box at the top, hit enter and you will start to see posts turn up. It won't go back and find existing posts, it just shows you new ones as fast as possible. Try following some fast moving tags like #bluesky to get going with it. The URL is bookmarkable and has a comma separated list of the tags you are following, e.g.
+    https://hashflow.apertum.ie/#bluesky,photography,nature
+You can click on hashtags in posts to subscribe to more posts mentioning that tag.
+
+## How it works
+The server connects to the Bluesky jetstream and watches every post get created. When your browser connects it provides the server with a list of hashtags you are interested in. When the server sees a post you have asked for it passes on the details.
--- hashflow.js
+++ hashflow.js
@@ -43,12 +43,18 @@ function decorate(skeet,facets){
             decorated=decorated+skeet.slice(stringcursor,start);
             decorated=decorated+"<strong class='hashtag' data-hashtag='"+tag+"'>#"+tag+"</strong>";
             cursor=facet.index.byteEnd;
-         }
-         if(facet.features[0].$type=='app.bsky.richtext.facet#link'){
+         }else if(facet.features[0].$type=='app.bsky.richtext.facet#link'){
             var uri=facet.features[0].uri;
             decorated=decorated+skeet.slice(stringcursor,start);
             decorated=decorated+"<a href='"+uri+"'>"+skeet.slice(start,end)+"</a>";
             cursor=facet.index.byteEnd;
+         }else if(facet.features[0].$type=='app.bsky.richtext.facet#mention'){
+            var did=facet.features[0].did;
+            decorated=decorated+skeet.slice(stringcursor,start);
+            decorated=decorated+"<i>"+skeet.slice(start,end)+"</i>"; //maybe link to the profile?
+            cursor=facet.index.byteEnd;
+         }else{
+            //console.log("Unprocessed facet " +facet.features[0].$type);
          }
          //if we don't recognise the facet don't move the cursor on.
      });
@@ -86,7 +92,25 @@ function renderskeet(m){
       }else{
           skeet=skeet+ "<p class='card-text'>"+ decorated +"</p>";
       }
-      addskeet(skeet,'white',1800000); //skeets last for 30 minutes
+      if (post.embed){
+         //there is one embed object, if it is type images
+         if (post.embed.$type=="app.bsky.embed.images#view"){
+             post.embed.images.forEach(function(image){
+               skeet=skeet+"<img class='attachedimage thumbnail' src='"+image.thumb+"'  alt='"+image.alt+"'>";
+//skeet=skeet+"<figure><figcaption>"+image.alt+"</figcaption><img height='200px' src='"+image.thumb+"'></figure>";
+
+
+             });
+         }else if(post.embed.$type=="app.bsky.embed.record#view"){
+                //this is a quoted post, they normally have some text, we could try harder.
+                //probably don't want to do this recursively and follow a massive quote thread.
+		skeet=skeet+"<hr>"+post.embed.record.value.text;
+         }else{
+             //console.log(post.embed);
+//             skeet=skeet+"<hr>Can't render "+post.embed.$type;
+         }
+      }
+      addskeet(skeet,'white',3600000); //skeets last for an hour. Might make this something user selected
    }
   });
 }
@@ -100,7 +124,6 @@ function opensocket(){
   });
   websocket.onopen=function(){
     addskeet("Connection ready","lightblue");
-    console.log("connection ready for data");
     window.tags.forEach(function(tag){
       //addskeet("Asking for #"+tag,"lightblue");
       sendsub(tag);
@@ -124,15 +147,33 @@ $(function(){
   opensocket();
   $('#tag').on("change",function(e){
     var hashlesstag=$('#tag').val().replace('#','');
-    //console.log("subscribing to "+hashlesstag);
     sendsub(hashlesstag);
     $('#tag').val('');
   });
-});
 
-document.addEventListener("click", function(e){
-  const target = e.target.closest(".hashtag"); // Or any other selector.
-  if(target){
-    sendsub($(target).data("hashtag"));
-  }
+  document.addEventListener("click", function(e){
+    const tag = e.target.closest(".hashtag"); // Or any other selector.
+    if(tag){
+      sendsub($(tag).data("hashtag"));
+    }
+    const image=e.target.closest(".attachedimage");
+    if(image){
+      $(image).toggleClass("thumbnail",500);
+    }
+
+  });
+  $("#About").on("click",function(e){
+    //playing with an idea here. Not sure it is a good one.
+    $.ajax({
+      url:"about.md",
+      success:function(data){
+        var converter = new showdown.Converter();
+        var html=converter.makeHtml(data);
+        $("<div>"+html+"</div>").dialog({minWidth: 500});
+      }
+    });
+    e.preventDefault();
+  });
+
+
 });
